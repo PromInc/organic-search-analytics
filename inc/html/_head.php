b43fa0866d5acc13e30854c56a4020a9f272b09d
PromInc <PromInc@hotmail.com>
2015-09-11 14:14:00 -0500

    FeedMe
    FeedMe button added

--- inc/html/_head.php
+++ inc/html/_head.php
@@ -19,14 +19,23 @@
 
 	<body>
 		<header>
-			<span><a href="index.php">Home</a></span>
-			<span><a href="data-capture.php">Data Capture</a></span>
-			<span><a href="settings.php">Settings</a></span>
-<!--
-			<span><a href="search.php">GWT Report Download</a></span>
-			<span><a href="import.php">Import</a></span>
--->
-			<span><a href="report.php">Reports</a></span>
+			<div class="floatleft">
+				<span><a href="index.php">Home</a></span>
+				<span><a href="data-capture.php">Data Capture</a></span>
+				<span><a href="settings.php">Settings</a></span>
+				<span><a href="report.php">Reports</a></span>
+			</div>
+			<div class="floatright">
+				<span class="donate">
+					<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+						<input type="hidden" name="cmd" value="_s-xclick">
+						<input type="hidden" name="hosted_button_id" value="52A4GD6S6QPG6">
+						<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+						<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+					</form>
+				</span>
+			</div>
+			<div class="clear"></div>
 		</header>
 
 		<div id="siteContent">
