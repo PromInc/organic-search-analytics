ff845a315931911b56c80155c6ffb6224c110868
PromInc <PromInc@hotmail.com>
2015-09-15 08:33:46 -0500

    Update config.php

--- organic-search-analytics/config/config.php
+++ organic-search-analytics/config/config.php
@@ -1,14 +1,14 @@
 <?php
 	class Config
 	{
-		const DB_CONNECTION_DOMAIN = 'localhost';
-		const DB_CONNECTION_USER = 'root';
-		const DB_CONNECTION_PASSWORD = 'root';
-		const DB_CONNECTION_DATABASE = 'organic-search-analytics';
+		const DB_CONNECTION_DOMAIN = '';
+		const DB_CONNECTION_USER = '';
+		const DB_CONNECTION_PASSWORD = '';
+		const DB_CONNECTION_DATABASE = '';
 
-		const OAUTH_CREDENTIALS_EMAIL = '292877011472-hpdacbef41q9od7neao2h720nj6eu0k4@developer.gserviceaccount.com';
-		const OAUTH_CREDENTIALS_PRIVATE_KEY_FILE_NAME = 'organic-search-analytics-f40021a76834.p12';
+		const OAUTH_CREDENTIALS_EMAIL = '';
+		const OAUTH_CREDENTIALS_PRIVATE_KEY_FILE_NAME = '';
 
-		const CREDENTIALS_BING_API_KEY = '8af9f38067434aa0899a392b7d041c5a';
+		const CREDENTIALS_BING_API_KEY = '';
 	}
 ?>
