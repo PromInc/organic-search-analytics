8604908dff7ef33fad948524f84aba17d592b4db
PromInc <PromInc@hotmail.com>
2015-09-15 08:34:34 -0500

    Config update

--- organic-search-analytics/config/config.php
+++ organic-search-analytics/config/config.php
@@ -3,11 +3,11 @@
 	{
 		const DB_CONNECTION_DOMAIN = 'localhost';
 		const DB_CONNECTION_USER = 'root';
-		const DB_CONNECTION_PASSWORD = 'root';
+		const DB_CONNECTION_PASSWORD = '';
 		const DB_CONNECTION_DATABASE = 'organic-search-analytics';
 
-		const OAUTH_CREDENTIALS_EMAIL = '292877011472-hpdacbef41q9od7neao2h720nj6eu0k4@developer.gserviceaccount.com';
-		const OAUTH_CREDENTIALS_PRIVATE_KEY_FILE_NAME = 'organic-search-analytics-f40021a76834.p12';
+		const OAUTH_CREDENTIALS_EMAIL = '1006616233802-365r94ic5u2843cb4v150jqefbkgm69t@developer.gserviceaccount.com';
+		const OAUTH_CREDENTIALS_PRIVATE_KEY_FILE_NAME = 'GWT Reporting-3f43fb4ca962.p12';
 
 		const CREDENTIALS_BING_API_KEY = '8af9f38067434aa0899a392b7d041c5a';
 	}
