7e697df0b6258178d03eb1c51bc1bd697ab0bf78
PromInc <PromInc@MacBook-Pro-PromInc.local>
2015-09-28 06:47:53 -0500

    Allow CRON Requests
    Parameters sent via a CRON request are different than a web request.
    Adjusting the data-capture-run.php code to accommodate.

--- organic-search-analytics/data-capture-run.php
+++ organic-search-analytics/data-capture-run.php
@@ -1,17 +1,23 @@
 <?php
 require_once realpath(dirname(__FILE__).'/inc/code/globalIncludes.php'); /* Load classes */
 
-if( isset($_GET) && isset($_GET['type']) && isset($_GET['domain']) && isset($_GET['date']) && preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$_GET['date']) ) {
+if( isset( $_GET ) ) {
+	$params = $_GET; /* Web Request */
+} else {
+	$params = $_SERVER['argv'][1]; /* CRON request */
+}
+
+if( isset($params) && isset($params['type']) && isset($params['domain']) && isset($params['date']) && preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$params['date']) ) {
 	/* Set the max allowed execution time for the page to allow for longer procesing times. */
 	ini_set('max_execution_time', 600);  //300 seconds = 5 minutes
 
 	/* Set overrides from URL paramters */
 	$overrideSettings = array();
-	if( isset( $_GET['mode'] ) ) { $overrideSettings['mode'] = $_GET['mode']; }
-	if( isset( $_GET['row_limit'] ) ) { $overrideSettings['row_limit'] = $_GET['row_limit']; }
-	if( isset( $_GET['dimensions'] ) ) { $overrideSettings['dimensions'] = explode( ',', $_GET['dimensions'] ); }
-	if( isset( $_GET['filters'] ) ) {
-		$filters = explode( '|', $_GET['filters'] );
+	if( isset( $params['mode'] ) ) { $overrideSettings['mode'] = $params['mode']; }
+	if( isset( $params['row_limit'] ) ) { $overrideSettings['row_limit'] = $params['row_limit']; }
+	if( isset( $params['dimensions'] ) ) { $overrideSettings['dimensions'] = explode( ',', $params['dimensions'] ); }
+	if( isset( $params['filters'] ) ) {
+		$filters = explode( '|', $params['filters'] );
 		foreach( $filters as $filter ) {
 			$filterArgs = explode( ',', $filter );
 			if( count( $filterArgs > 1 ) ) {
@@ -21,24 +27,24 @@ if( isset($_GET) && isset($_GET['type']) && isset($_GET['domain']) && isset($_GE
 			}
 		}
 	}
-	if( isset( $_GET['aggregation_type'] ) ) { $overrideSettings['aggregation_type'] = $_GET['aggregation_type']; }
+	if( isset( $params['aggregation_type'] ) ) { $overrideSettings['aggregation_type'] = $params['aggregation_type']; }
 
-	switch( $_GET['type'] ) {
+	switch( $params['type'] ) {
 		case 'googleSearchAnalytics':
-			$recordsImported = $dataCapture->downloadGoogleSearchAnalytics($_GET['domain'],$_GET['date'], $overrideSettings);
-			if( !isset( $_GET['mode'] ) || $_GET['mode'] != 'return' ) {
-				echo number_format( $recordsImported ) . " records succesfully imported to the database for " . $_GET['domain'] . " for date: " . $_GET['date'] . ".";
+			$recordsImported = $dataCapture->downloadGoogleSearchAnalytics($params['domain'],$params['date'], $overrideSettings);
+			if( !isset( $params['mode'] ) || $params['mode'] != 'return' ) {
+				echo number_format( $recordsImported ) . " records succesfully imported to the database for " . $params['domain'] . " for date: " . $params['date'] . ".";
 			}
 			break;
 		case 'bingSearchKeywords':
-			$recordsImported = $dataCapture->downloadBingSearchKeywords($_GET['domain'],$_GET['date'], $overrideSettings);
-			if( !isset( $_GET['mode'] ) || $_GET['mode'] != 'return' ) {
-				echo number_format( $recordsImported ) . " records succesfully imported to the database for " . $_GET['domain'] . ".";
+			$recordsImported = $dataCapture->downloadBingSearchKeywords($params['domain'],$params['date'], $overrideSettings);
+			if( !isset( $params['mode'] ) || $params['mode'] != 'return' ) {
+				echo number_format( $recordsImported ) . " records succesfully imported to the database for " . $params['domain'] . ".";
 			}
 			break;
 	}
 
 } else {
-	echo "<p>ERROR: Invalid request.  Domain: " . $_GET['domain'] . ", Date: " . $_GET['date'] . "</p>";
+	echo "<p>ERROR: Invalid request.  Domain: " . $params['domain'] . ", Date: " . $params['date'] . "</p>";
 }
 ?>
